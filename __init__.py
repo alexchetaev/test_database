74b54525e97214b3efe49cd185d1b1dba6a65407
Aleksey Chetaev <alex.chetaev@gmail.com>
2020-04-08 22:53:07 +0300

    Add test with table data comparing

--- __init__.py
+++ __init__.py
@@ -6,27 +6,66 @@ This file create structure in database and need only for init structure for test
 
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.session import sessionmaker
 
 
 Base = declarative_base()
 
+internal_users = ["Jone", "Bill", "Bella", "Jake"]
 
-class SomeObject(Base):
+external_users = ["Jone", "Bill", "Bella", "Jake", "Stupid Guy"]
+
+
+class InternalUser(Base):
     """
     Base class for test table structure
     """
 
-    __tablename__ = 'some_objects'
+    def __init__(self, _name=None):
+        self.name = _name
 
-    id = Column(Integer, primary_key=True)
+    __tablename__ = 'internal_users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    name = Column(String(50))
+
+
+class ExternalUser(Base):
+    """
+    Base class for test table structure
+    """
+
+    def __init__(self, _name=None):
+        self.name = _name
+
+    __tablename__ = 'external_users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
 
     name = Column(String(50))
 
 
 engine = create_engine('mysql://test_user:test_password@localhost/test_db')
 
+Session = sessionmaker()
+
+Session.configure(bind=engine)
+
+session = Session()
+
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
 
+    for name in internal_users:
+        o = InternalUser(name)
 
+        session.add(o)
+
+    for name in external_users:
+        o = ExternalUser(name)
+
+        session.add(o)
+
+    session.commit()
 
--- tests/test_database.py
+++ tests/test_database.py
@@ -21,23 +21,30 @@ test_db_tables: dict = meta_data.tables
 Structure storage should be in separate file but now here if for example
 """
 tables_structures = {
-    "some_objects": {
+    "internal_users": {
         "id": sqlalchemy.dialects.mysql.types.INTEGER,
         "name": {
             "type": sqlalchemy.dialects.mysql.types.VARCHAR,
             "length": 50,
         }
-    }
+    },
+    "external_users": {
+        "id": sqlalchemy.dialects.mysql.types.INTEGER,
+        "name": {
+            "type": sqlalchemy.dialects.mysql.types.VARCHAR,
+            "length": 50,
+        }
+    },
 }
 ################
 
 
-@pytest.mark.skip(reason="abstract function")
+@pytest.mark.skip(reason="Abstract function")
 def test_table_exist(table_name, tables_list):
     assert (table_name in tables_list), f"Error {table_name} did not found in database"
 
 
-@pytest.mark.skip(reason="abstract function")
+@pytest.mark.skip(reason="Abstract function")
 def test_table_structure(table, structure, check_string_length=False):
     for k, v in structure.items():
         assert (k in table._columns), f"Column {k} did not found in table {table.name}"
@@ -52,6 +59,49 @@ def test_table_structure(table, structure, check_string_length=False):
             assert (v.get('length') == column.type.length), (f"Bad varchar column length. Should be {v.get('length')} "
                                                              f"but now {column.type.length}")
 
+        """
+        In general may be we need to add something another specific check as we added for string length for another 
+        column types that we use in our database. 
+        """
+
+
+@pytest.mark.skip(reason="Abstract function")
+def test_data_matching(source_cursor, target_cursor: sqlalchemy.engine.result.ResultProxy, id_column_name, columns=None):
+    """
+    This can be optimized without for in for and id. This code is only for example
+
+    :param source_cursor: reference data
+    :param target_cursor: test data
+    """
+    source_cursor = list(source_cursor)
+
+    target_cursor = list(target_cursor)
+
+    for source_row in source_cursor:
+        found = False
+
+        checked = True
+
+        problem_colums = []
+
+        for target_row in target_cursor:
+            if source_row[id_column_name] == target_row[id_column_name]:
+                found = True
+
+                for k in source_row._keymap:
+                    if source_row[k] == target_row[k]:
+                        continue
+                    else:
+                        problem_colums.append(k)
+
+                        checked = False
+
+                break
+
+        assert found, f"Row with id {source_row[id_column_name]} did not found in target tables"
+
+        assert checked, f"Data do not mach for columns {problem_colums} in row with id {source_row[id_column_name]}"
+
 
 def test_tables_structure():
     for k, v in tables_structures.items():
@@ -59,7 +109,15 @@ def test_tables_structure():
 
         table = test_db_tables.get(k)
 
-        test_table_structure(table, v)
+        test_table_structure(table, v, check_string_length=True)
+
+
+def test_tables_data():
+    target_cursor = test_db_conn.execute("select * from internal_users")
+
+    source_cursor = test_db_conn.execute("select * from external_users")
+
+    test_data_matching(source_cursor, target_cursor, "id")
 
 
 
